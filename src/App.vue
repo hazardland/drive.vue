adc92c37be0193ea3486142bf41cafa22b9fad69
vakhtang <hazardland@gmail.com>
2020-10-05 01:14:20 +0400

    polished

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,6 @@
   </div>
 </template>
 
-<script>
-    export default {
-    metaInfo () {
-        return {
-            meta: [
-                {property: 'og:title', content: 'მართვის მოწმობის თეორიის ონლაინ მასწავლებელი'},
-                {property: 'og:site_name', content: 'მართვის მოწმობა'},
-                {property: 'og:type', content: 'website'},
-                {property: 'og:image', content: 'https://raw.githubusercontent.com/hazardland/drive.vue/master/readme.png'},
-                {property: 'og:description', content: 'აპლიკაცია იმახსოვრებს ბილეთებს რომელშიც იჭრებით და იძლევა ცალკე დაფილტვრის საშუალებას, ნასწავლ ბილეთად თვლის ისეთ ბილეთს რომელზეც სხვადასხვა დროს 3-ჯერ ზედიზედ სწორად გაეცით პასუხი, ასევე თვალსაჩინოდ ასახავს თავების სწავლის პროცესს სხვადასხვა ინდიკატორებით. როდესაც ყველა თავის ქვეშ ყველა პროგრესბარი მთლიანად გამწვანდება შეგიძლიათ თამამად გახვიდეთ გამოცდაზე.'}
-                ]
-            }
-        }
-    }
-</script>
-
 <style lang="scss">
     html {
         font-family: Verdana
--- src/main.js
+++ src/main.js
@@ -2,14 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Meta from 'vue-meta'
 import tickets from '@/data/tickets.json'
 import categories from '@/data/categories.json'
 import subjects from '@/data/subjects.json'
 // import Learn from '@/lib/learn'
 
 Vue.config.productionTip = false
-Vue.use(Meta)
 
 Vue.prototype.tickets = tickets
 Vue.prototype.subjects = subjects
